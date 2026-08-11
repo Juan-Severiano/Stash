76aca6b2a13ca79cd0e833e48a06694ba33140d3
Francisco Juan <juansoussev@gmail.com>
2026-08-11 12:48:43 -0300

    Initial Commit

---
+++ Stash/StashApp.swift
@@ -0,0 +1,17 @@
+//
+//  StashApp.swift
+//  Stash
+//
+//  Created by Francisco Juan on 11/08/26.
+//
+
+import SwiftUI
+
+@main
+struct StashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
